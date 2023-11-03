a4459f1487c3c56d4edee6595919a07da3ff07ef
Ryan Williams <git@ryan.nz>
2023-11-04 11:50:51 +1300

    Add GLFW and open a window

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <plonk/plonk.h>
+#include <GLFW/glfw3.h>
 
 int main(int, char **) {
-	std::cout << "1 + 2 = " << plonk::sum(1, 2) << '\n';
+	std::cout << "Starting Plonk...\n";
+
+	if (!glfwInit()) {
+		std::cout << "Error initialising glfw\n";
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Plonker", NULL, NULL);
+	if (!window) {
+		std::cout << "Error creating window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	std::cout << "Finished Plonk\n";
 	return 0;
 }
